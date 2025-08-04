cb06ea5a9cc5aa9aea5286234db6cdcb52fa6bd7
Muhit2004 <themuhit123@gmail.com>
2025-08-05 02:50:45 +0600

    fsfs

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/my-portfolio/'
+  base : import.meta.env.VITE_BASE_PATH || "/my-portfolio",
+ 
 })
